a6262fd82e6173535858f5366eae2205a4a245cf
Erickson Juang <a820224@gmail.com>
2017-04-13 08:22:25 +0800

    Add LoginActivity

---
+++ app/src/main/java/tw/bingluen/heyyzu/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package tw.bingluen.heyyzu.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tw.bingluen.heyyzu.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
