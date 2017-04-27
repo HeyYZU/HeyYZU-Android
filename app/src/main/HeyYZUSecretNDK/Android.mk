633aad2ed1f7fecce651944f91e6f66301e2a150
Erickson Juang <a820224@gmail.com>
2017-04-27 15:35:52 +0800

    Add API NDK & v13-support-library

---
+++ app/src/main/HeyYZUSecretNDK/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := HeyYZUSecret
+
+LOCAL_SRC_FILES := tw_bingluen_heyyzu_constant_HeyYZUSecret.cpp
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ app/src/main/HeyYZUSecretNDK/sample_tw_bingluen_heyyzu_constant_HeyYZUSecret.cpp
@@ -0,0 +1,11 @@
+#include "tw_bingluen_heyyzu_constant_HeyYZUSecret.h"
+#include <string.h>
+#include <jni.h>
+
+jstring Java_tw_bingluen_heyyzu_constant_HeyYZUSecret_getCipher(JNIEnv *env, jclass that) {
+    return (env)->NewStringUTF("");
+}
+
+jstring Java_tw_bingluen_heyyzu_constant_HeyYZUSecret_getSecret(JNIEnv *env, jclass that) {
+    return (env)->NewStringUTF("");
+}
