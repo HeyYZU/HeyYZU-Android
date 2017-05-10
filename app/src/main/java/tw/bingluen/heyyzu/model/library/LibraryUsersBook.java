674a9dac4a8b386ac9c4180e71ec85a810388ddf
Erickson Juang <a820224@gmail.com>
2017-05-10 21:50:07 +0800

    Update model

--- app/src/main/java/tw/bingluen/heyyzu/model/library/LibraryUsersBook.java
+++ app/src/main/java/tw/bingluen/heyyzu/model/library/LibraryUsersBook.java
@@ -1,17 +1,16 @@
 package tw.bingluen.heyyzu.model.library;
 
-import java.util.List;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class LibraryUsersBook extends LibraryBookBase {
     @SerializedName("attr")
     @Expose
-    private List<LibraryBookAttribute> attr = null;
-    public List<LibraryBookAttribute> getAttr() {
+    private LibraryBookAttribute attr = null;
+    public LibraryBookAttribute getAttr() {
         return attr;
     }
-    public void setAttr(List<LibraryBookAttribute> attr) {
+    public void setAttr(LibraryBookAttribute attr) {
         this.attr = attr;
     }
 }
