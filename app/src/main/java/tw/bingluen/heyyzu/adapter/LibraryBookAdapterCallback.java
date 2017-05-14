27bea1b14166f7d2d996faa9a811f9e3dc2f386a
Erickson Juang <a820224@gmail.com>
2017-05-14 18:56:13 +0800

    Remove comment

--- app/src/main/java/tw/bingluen/heyyzu/adapter/LibraryBookAdapterCallback.java
+++ app/src/main/java/tw/bingluen/heyyzu/adapter/LibraryBookAdapterCallback.java
@@ -4,9 +4,6 @@ import android.view.View;
 
 import tw.bingluen.heyyzu.model.library.LibraryUsersBook;
 
-/**
- * Created by zhuangbinglun on 2017/5/11.
- */
 
 public interface LibraryBookAdapterCallback {
     void onItemClick(View v, int pos, LibraryUsersBook book);
